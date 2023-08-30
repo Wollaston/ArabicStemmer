172b3b8707613097402b483a4c02799baf9c459b
Wollaston <mattflynn1997@gmail.com>
2023-08-30 12:08:44 -0400

    Update project name and basic styling

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,10 @@
 <script>
-	import { enhance } from '$app/forms';
+	const imageURL = '/github-mark-white.svg';
 </script>
 
 <div class="w-full bg-base-300 fixed bottom-0 flex flex-row content-center justify-center h-24">
-	<img
-		class="object-scale-down h-12 self-center m-2"
-		src="src/lib/assets/github-mark-white.svg"
-		alt="GitHub.com"
-	/>
-	<a href="https://github.com/Wollaston/AraStemmerApp" class="self-center text-lg">AraStemmer</a>
+	<a href="https://github.com/Wollaston/ArabicStemmer" class="self-center"
+		><img class="object-scale-down h-12 self-center m-2" src={imageURL} alt="GitHub.com" /></a
+	>
+	<a href="https://github.com/Wollaston/ArabicStemmer" class="self-center text-lg">ArabicStemmer</a>
 </div>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,11 +1,14 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div class="navbar bg-base-300">
 	<div class="flex-1">
-		<a href="/" class="btn btn-ghost normal-case text-xl">AraStemmer</a>
+		<a href="{base}/" class="btn btn-ghost normal-case text-xl">ArabicStemmer</a>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal px-1">
-			<li><a href="/guide">Guide</a></li>
-			<li><a href="/about">About</a></li>
+			<li><a href="{base}/about">About</a></li>
 		</ul>
 	</div>
 </div>
